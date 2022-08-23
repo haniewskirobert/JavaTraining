94c343892ce24e225d29d9ba1f764fb67fba6f1f
pkarpiel <pkarpiel@sii.pl>
2022-08-23 11:08:15 +0200

    ex 8

--- Basics/src/main/java/com/testinglaboratory/basics/lesson8/exceptions/ExceptionExercise.java
+++ Basics/src/main/java/com/testinglaboratory/basics/lesson8/exceptions/ExceptionExercise.java
@@ -1,8 +1,38 @@
 package com.testinglaboratory.basics.lesson8.exceptions;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //TODO create list, map and array with example values
 // try to access nonexistent indexes or keys
 // see what happens, and think what can be done to fix it
 public class ExceptionExercise {
+    public static void main(String[] args) {
+        try{
+            int[] x = {5, 78, 245, 690, 554, 22113};
+            System.out.println(x.length);
+        }catch (Exception exception){
+            System.out.println(exception.getCause());
+            System.out.println(exception.getMessage());
+            System.out.println(exception.getStackTrace());
+        }
 
+        List<Integer> num = List.of(64, 298, 90, 26, 1);
+        List<Integer> listofNum = new ArrayList<Integer>() {{
+            add(1);
+            add(343);
+            add(896845);
+            add(23423);
+            add(234);
+        }};
+
+       // System.out.println(x.length);
+        System.out.println(num.toString());
+        System.out.println(listofNum);
+
+
+
+    }
 }
--- Basics/src/main/java/com/testinglaboratory/basics/lesson9_loops/ForEachLoop.java
+++ Basics/src/main/java/com/testinglaboratory/basics/lesson9_loops/ForEachLoop.java
@@ -6,16 +6,29 @@ import java.util.List;
 public class ForEachLoop {
 
     public static void main(String[] args) {
-        List<String> personLists = new ArrayList<>();
-        personLists.add("Josh");
-        personLists.add("Anna");
-        personLists.add("Milfred");
+        List<String> people = new ArrayList<>();
 
+        people.add("Mateusz");
+        people.add("Anna");
+        people.add("Paulina");
 
-        for (String person : personLists) {
-            System.out.println(person);
+        System.out.println("for loop");
+        for (int i = 0; i < people.size(); i++) {
+            String person = people.get(i);
+            if (person.length() == 4) {
+                System.out.println(person);
+            }
+        }
+
+        System.out.println("for each loop");
+        for (String person : people) {
+            if (person.length() == 4) {
+                System.out.println(person);
+            }
         }
     }
 
     //todo try to copy to other list only those person who name have 4 letters.
+
+
 }
